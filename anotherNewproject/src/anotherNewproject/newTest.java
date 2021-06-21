01bb174a37d2f4b96f6cfc9a209029bcfc3d152a
zparilti <zparilti@gmail.com>
2021-06-20 22:08:08 -0400

    update on

--- anotherNewproject/src/anotherNewproject/newTest.java
+++ anotherNewproject/src/anotherNewproject/newTest.java
@@ -9,6 +9,14 @@ public class newTest {
 		System.out.println("THIS IS COOL");
 		System.out.println("Another cool line and this is COOL too");
 		
+		// The following lines is about adding two number together
+		
+		int th = 5;
+		int nh = 10;
+		
+		System.out.println("5 + 10 = "+(th+nh));
+		
+		
 		
 	}
 
